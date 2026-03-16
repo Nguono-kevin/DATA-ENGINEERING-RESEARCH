bccc011080ee79886ec4419608e11846a3b0fb1a
kevin nguono odhiambo <kevinnguono19@gmail.com>
2026-03-16 11:45:50 +0300

    commit

--- spark_streaming/weather_consumer.py
+++ spark_streaming/weather_consumer.py
@@ -1,19 +1,60 @@
+# weather_consumer.py
+import os
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import *
-from schema import weather_schema
+from pyspark.sql.functions import from_json, col
+from pyspark.sql.types import StructType, StructField, StringType, DoubleType, IntegerType, LongType
 
-spark=SparkSession.builder \
-.appName("KenyaWeatherPipeline") \
-.getOrCreate()
+# ---------------------------
+# 1. Windows Hadoop setup
+# ---------------------------
+os.environ["HADOOP_HOME"] = "C:\\hadoop"
+os.environ["HADOOP_CONF_DIR"] = "C:\\hadoop\\etc\\hadoop"
 
-df=spark.readStream \
-.format("kafka") \
-.option("kafka.bootstrap.servers","localhost:9092") \
-.option("subscribe","kenya_weather") \
-.load()
+# ---------------------------
+# 2. Define Spark session
+# ---------------------------
+spark = SparkSession.builder \
+    .appName("KafkaWeatherConsumer") \
+    .getOrCreate()
 
-json_df=df.selectExpr("CAST(value AS STRING)")
+# ---------------------------
+# 3. Define Kafka topic schema
+# ---------------------------
+weather_schema = StructType([
+    StructField("region", StringType(), True),
+    StructField("city", StringType(), True),
+    StructField("temperature", DoubleType(), True),
+    StructField("humidity", IntegerType(), True),
+    StructField("wind_speed", DoubleType(), True),
+    StructField("weather", StringType(), True),
+    StructField("timestamp", LongType(), True)
+])
 
-weather_df=json_df.select(
-from_json(col("value"),weather_schema).alias("data")
-).select("data.*")
+# ---------------------------
+# 4. Read streaming data from Kafka
+# ---------------------------
+df = spark.readStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", "localhost:9092") \
+    .option("subscribe", "kenya_weather") \
+    .option("startingOffsets", "latest") \
+    .load()
+
+# Kafka message value is in binary, convert to string
+df_str = df.selectExpr("CAST(value AS STRING)")
+
+# Parse JSON into structured columns
+df_parsed = df_str.withColumn("data", from_json(col("value"), weather_schema)) \
+                  .select("data.*")
+
+# ---------------------------
+# 5. Output streaming to console
+# ---------------------------
+query = df_parsed.writeStream \
+    .format("console") \
+    .outputMode("append") \
+    .option("truncate", False) \
+    .option("checkpointLocation", "C:\\Spark\\checkpoints\\weather") \
+    .start()
+
+query.awaitTermination()
